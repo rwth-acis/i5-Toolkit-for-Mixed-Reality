d7a3a549c729b1bef0313a9b5ce9508ed6081275
SebastianMeinberger <sebastian.meinberger@rwth-aachen.de>
2021-03-21 20:53:17 +0100

    Added a coustom insepector for the ServiceBootstraper

---
+++ Assets/i5 Toolkit for Mixed Reality/Editor/PieMenuEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(PieMenuServiceBootstraper))]
+public class PieMenuEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        //base.OnInspectorGUI();
+        PieMenuServiceBootstraper pieMenu = (PieMenuServiceBootstraper)target;
+        EditorGUILayout.LabelField("Color Options");
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("toolSetup.pieMenuPieceNormalColor"),new GUIContent("Color of the PieMenu"));
+        EditorGUILayout.LabelField("Actions");
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("toolSetup.triggerInputAction"), new GUIContent("Trigger Input Action"));
+    }
+}
