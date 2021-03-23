94cf3de1dad0b4019c60c7ca82805ac3d6b7b287
SebastianMeinberger <sebastian.meinberger@rwth-aachen.de>
2021-03-23 11:27:30 +0100

    The fields are now placed beside buttons that switch the category

--- Assets/i5 Toolkit for Mixed Reality/Editor/PieMenuEditor.cs
+++ Assets/i5 Toolkit for Mixed Reality/Editor/PieMenuEditor.cs
@@ -6,13 +6,46 @@ using UnityEngine;
 [CustomEditor(typeof(PieMenuServiceBootstraper))]
 public class PieMenuEditor : Editor
 {
+    int state = 0;
     public override void OnInspectorGUI()
     {
-        //base.OnInspectorGUI();
         PieMenuServiceBootstraper pieMenu = (PieMenuServiceBootstraper)target;
-        EditorGUILayout.LabelField("Color Options");
-        EditorGUILayout.PropertyField(serializedObject.FindProperty("toolSetup.pieMenuPieceNormalColor"),new GUIContent("Color of the PieMenu"));
-        EditorGUILayout.LabelField("Actions");
-        EditorGUILayout.PropertyField(serializedObject.FindProperty("toolSetup.triggerInputAction"), new GUIContent("Trigger Input Action"));
+
+        EditorGUILayout.BeginHorizontal();
+
+        EditorGUILayout.BeginVertical(GUILayout.MaxWidth(30));
+
+        if (GUILayout.Button("Apperance"))
+        {
+            state = 0;
+        }
+        if (GUILayout.Button("Actions"))
+        {
+            state = 1;
+        }
+
+        EditorGUILayout.EndVertical();
+
+        EditorGUILayout.BeginVertical();
+
+        if (state == 0)
+        {
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("toolSetup.pieMenuPieceNormalColor"), new GUIContent("Color of the PieMenu"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("toolSetup.pieMenuPieceHighlighColor"), new GUIContent("Color of highlighted pieces"));
+        }
+        else
+        {
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("toolSetup.triggerInputAction"), new GUIContent("Trigger input action"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("toolSetup.touchpadTouchActionAction"), new GUIContent("Touchpad touch action"));
+        }
+
+        EditorGUILayout.EndVertical();
+        EditorGUILayout.EndHorizontal();
     }
+}
+
+public enum PieInsepectoState
+{
+    Apperace,
+    Actions
 }
