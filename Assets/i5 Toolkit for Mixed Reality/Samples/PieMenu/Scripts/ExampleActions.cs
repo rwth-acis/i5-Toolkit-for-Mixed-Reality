ef08173e2fa9f2a5ac759539953df5fd2957ae0f
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2021-04-27 20:18:03 +0200

    Added some test actions

---
+++ Assets/i5 Toolkit for Mixed Reality/Samples/PieMenu/Scripts/ExampleActions.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace i5.Toolkit.MixedReality.PieMenu
+{
+    public class ExampleActions : ActionHelperFunctions
+    {
+        public void TouchPadRightTest()
+        {
+            Debug.Log("Tochpad Right Pressed");
+        }
+
+        public void TouchPadUpTest()
+        {
+            Debug.Log("Tochpad Up Pressed");
+        }
+
+        public void TouchPadLeftTest()
+        {
+            Debug.Log("Tochpad Left Pressed");
+        }
+
+        public void TouchPadDownTest()
+        {
+            Debug.Log("Tochpad Down Pressed");
+        }
+
+        public void TriggerPressTest()
+        {
+            Debug.Log("Trigger Pressed");
+        }
+
+        public void GripPressTest()
+        {
+            Debug.Log("Grip Pressed");
+        }
+    }
+}
