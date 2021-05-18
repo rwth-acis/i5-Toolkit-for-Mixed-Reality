74c46a34e4c033fe2448b7e18a79a12098aa9038
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2021-05-18 16:24:43 +0200

    Added a delete action to the sample scene

---
+++ Assets/i5 Toolkit for Mixed Reality/Samples/PieMenu/Scripts/DeleteActionWrapper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Input;
+using i5.Toolkit.MixedReality.PieMenu;
+using i5.Toolkit.Core.ServiceCore;
+
+public class DeleteActionWrapper : MonoBehaviour
+{
+    public void Delete(BaseInputEventData data)
+    {
+        IObjectTransformer objectTransformer = FindObjectOfType<ObjectTransformer>().GetComponent<ObjectTransformer>();
+        GameObject target = ActionHelperFunctions.GetTargetFromInputSource(data.InputSource);
+        //string toolName = ActionHelperFunctions.GetVirtualToolFromPointer(data.InputSource.Pointers[0]).currentEntry.toolSettings.toolName;
+        target = objectTransformer.transformObject(target, "Delete");
+
+        if (target != null)
+        {
+            ServiceManager.GetService<CommandStackService>().AddAndPerformAction(new DeleteAction(target));
+        }
+    }
+}
---
+++ Assets/i5 Toolkit for Mixed Reality/Samples/PieMenu/Scripts/ObjectTransformer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using i5.Toolkit.MixedReality.PieMenu;
+
+public class ObjectTransformer : MonoBehaviour, IObjectTransformer
+{
+    GameObject IObjectTransformer.transformObject(GameObject objectToTransform, string toolName)
+    {
+        switch (toolName)
+        {
+            case "Delete":
+                return ActionHelperFunctions.GetGameobjectOfTypeFromHirachy(objectToTransform, typeof(SampleObject));
+            default:
+                return null;
+
+        }
+    }
+}
