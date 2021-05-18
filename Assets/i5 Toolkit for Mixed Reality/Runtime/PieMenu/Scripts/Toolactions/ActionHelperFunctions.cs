34036a265016d618f3697eaa4e62feb266113d36
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2021-05-18 16:21:40 +0200

    Added a GetTargetFromInputSource function

--- Assets/i5 Toolkit for Mixed Reality/Runtime/PieMenu/Scripts/Toolactions/ActionHelperFunctions.cs
+++ Assets/i5 Toolkit for Mixed Reality/Runtime/PieMenu/Scripts/Toolactions/ActionHelperFunctions.cs
@@ -11,7 +11,7 @@ namespace i5.Toolkit.MixedReality.PieMenu
     public class ActionHelperFunctions : MonoBehaviour
     {
         /// <summary>
-        /// Iterates upwards in the hirachy of gameObject and returns the first GameObject that has the Visualization script attached. Returns null if nothing is found.
+        /// Iterates upwards in the hirachy of gameObject and returns the first GameObject that has a script of type typeToSearch attached. Returns null if nothing is found.
         /// </summary>
         /// <param name="gameObject"></param> The gameObject on which the search should start
         /// <param name="typesToExclude"></param> Can be used as a filter. A GameObject can be ignored if it has a object contianed in typesToExclude above or below it
@@ -79,6 +79,24 @@ namespace i5.Toolkit.MixedReality.PieMenu
             //Set the x and y rotation to 0 and flip it around because with LookAt, the prefab faces away from the camera and is tilted strangly
             Vector3 rotation = objectToRotate.transform.eulerAngles;
             objectToRotate.transform.SetPositionAndRotation(newPosition, Quaternion.Euler(0, 180 + rotation.y, 0));
+        }
+
+        /// <summary>
+        /// When the input source focuses a target, the target is returned. Otherwise null is returned.
+        /// </summary>
+        /// <param name="inputSource"></param> The input source
+        /// <returns></returns>
+        public static GameObject GetTargetFromInputSource(IMixedRealityInputSource inputSource)
+        {
+            foreach (var pointer in inputSource.Pointers)
+            {
+                GameObject target = pointer.Result?.CurrentPointerTarget;
+                if (target != null)
+                {
+                    return target;
+                }
+            }
+            return null;
         }
     } 
 }
