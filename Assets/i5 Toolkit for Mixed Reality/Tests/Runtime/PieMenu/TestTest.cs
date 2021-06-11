a75db6032dda353301e222fd8aaff6a60fe38d25
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2021-06-11 20:54:57 +0200

    Added some test infrastructure

---
+++ Assets/i5 Toolkit for Mixed Reality/Tests/Runtime/PieMenu/TestTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using NUnit.Framework;
+
+public class TestTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
