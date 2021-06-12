cda29d9a3ce8735bed59d4045536d1188caca1a9
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2021-06-12 17:42:40 +0200

    Made rotateToCamera static

--- Assets/i5 Toolkit for Mixed Reality/Runtime/PieMenu/Scripts/Toolactions/ActionHelperFunctions.cs
+++ Assets/i5 Toolkit for Mixed Reality/Runtime/PieMenu/Scripts/Toolactions/ActionHelperFunctions.cs
@@ -91,7 +91,7 @@ namespace i5.Toolkit.MixedReality.PieMenu
         /// </summary>
         /// <param name="objectToRotate"></param> The object to move and rotate
         /// <param name="newPosition"></param> The new position
-        public void RotateToCameraOnXZPlane(GameObject objectToRotate, Vector3 newPosition)
+        public static void RotateToCameraOnXZPlane(GameObject objectToRotate, Vector3 newPosition)
         {
             objectToRotate.transform.LookAt(Camera.main.transform);
             //Set the x and y rotation to 0 and flip it around because with LookAt, the prefab faces away from the camera and is tilted strangly
