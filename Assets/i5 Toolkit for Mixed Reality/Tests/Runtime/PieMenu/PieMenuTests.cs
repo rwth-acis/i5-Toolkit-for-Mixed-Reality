678aea8bfd7ab036ff48c59ff794001c0494e305
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2021-06-15 16:28:21 +0200

    The tests now load a specific test scene on startup

---
+++ Assets/i5 Toolkit for Mixed Reality/Tests/Runtime/PieMenu/PieMenuTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using FakeItEasy;
+using UnityEngine.TestTools;
+using i5.Toolkit.Core.TestHelpers;
+using i5.Toolkit.MixedReality.PieMenu;
+using i5.Toolkit.Core.ServiceCore;
+using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
+using Microsoft.MixedReality.Toolkit;
+
+namespace i5.Toolkit.MixedReality.Tests.PieMenu {
+    public class PieMenuTests
+    {
+        [SetUp]
+        public void LoadScene()
+        {
+            var sceneParameter = new LoadSceneParameters { loadSceneMode = LoadSceneMode.Single, localPhysicsMode = LocalPhysicsMode.Physics3D};
+            SceneManager.LoadScene("PieMenuTestScene");
+        }
+
+        [TearDown]
+        public void TearDownScene()
+        {
+            SceneManager.UnloadScene("PieMenuTestScene");
+        }
+
+        [UnityTest]
+        public IEnumerator ServiceManager_can_return_toolSetupService()
+        {
+            yield return null;
+            var toolSetupService = ServiceManager.GetService<ToolSetupService>();
+            Assert.IsNotNull(toolSetupService, "ToolSetupService");
+        }
+
+        [UnityTest]
+        public IEnumerator Can_spawn_PieMenu()
+        {
+            var pieMenuManager = GameObject.FindObjectOfType<PieMenuManager>();
+            //pieMenuManager.op
+            var test = GameObject.FindObjectOfType<MixedRealityToolkit>();
+            
+            
+
+            return null;
+        }
+
+        
+    }
+}
---
+++ Assets/i5 Toolkit for Mixed Reality/Tests/Runtime/PieMenu/TempTestUtiltys.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Microsoft.MixedReality.Toolkit.Input;
+
+/// <summary>
+/// Theroratically inckuded in the i5 toolkit. Only included here as workaround.
+/// </summary>
+public static class PlayModeTestUtilities
+{
+    public static void LoadEmptyTestScene()
+    {
+        CheckPlayMode();
+        Scene playScene = SceneManager.CreateScene("PlayTest Scene");
+        SceneManager.SetActiveScene(playScene);
+    }
+
+    public static void UnloadTestScene()
+    {
+        CheckPlayMode();
+        SceneManager.UnloadScene("PlayTest Scene");
+    }
+
+    private static void CheckPlayMode()
+    {
+        if (!Application.isPlaying)
+        {
+            throw new InvalidPlatformException("Play Mode Test Utilities can only be used in PlayMode");
+        }
+    }
+
+    public static BaseInputEventData FakeBaseInputEvent()
+    {
+        return null;
+    }
+}
+
+public class InvalidPlatformException : Exception
+{
+    public InvalidPlatformException()
+    {
+    }
+
+    public InvalidPlatformException(string message) : base(message)
+    {
+    }
+
+    public InvalidPlatformException(string message, Exception inner) : base(message, inner)
+    {
+    }
+}
