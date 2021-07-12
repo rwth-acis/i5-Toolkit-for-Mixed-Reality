a709c1bec30d0eee1d292a6968306738e8eb33c3
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2021-07-12 20:46:09 +0200

    Added the missing interface implemenatations for the RendererShell

--- Assets/i5 Toolkit for Mixed Reality/Runtime/PieMenu/Scripts/PieMenuRenderer/PieMenuRendererShell.cs
+++ Assets/i5 Toolkit for Mixed Reality/Runtime/PieMenu/Scripts/PieMenuRenderer/PieMenuRendererShell.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,7 +7,7 @@ using i5.Toolkit.Core.ServiceCore;
 
 namespace i5.Toolkit.MixedReality.PieMenu
 {
-    /*
+
     public class PieMenuRendererShell : MonoBehaviour, IPieMenuRendererShell
     {
         PieMenuRendererCore core;
@@ -20,7 +20,8 @@ namespace i5.Toolkit.MixedReality.PieMenu
 
         public List<MenuEntry> menuEntries { get; private set; }
         public List<GameObject> pieMenuPieces { get; private set; }
-        public int currentlyHighlighted;
+
+        int currentlyHighlighted;
 
         GameObject instantiatedMenuCursor;
 
@@ -28,15 +29,22 @@ namespace i5.Toolkit.MixedReality.PieMenu
 
         PieMenuSetup toolSetup;
 
-        void Start()
+        public void Constructor(IMixedRealityPointer pointer)
         {
             toolSetup = ServiceManager.GetService<ToolSetupService>().toolSetup;
+            this.pointer = pointer;
+            pieMenuPieces = new List<GameObject>();
             core = new PieMenuRendererCore(toolSetup, this, ref currentlyHighlighted);
         }
 
         void Update()
         {
-            core.Update(pointer, toolSetup, ref currentlyHighlighted);
+            core?.Update(pointer, toolSetup, ref currentlyHighlighted);
+        }
+
+        public int getCurrentlyHighlighted()
+        {
+            return currentlyHighlighted;
         }
 
         //Self
@@ -57,6 +65,75 @@ namespace i5.Toolkit.MixedReality.PieMenu
             pieMenuPieces.Add(piece);
         }
 
+        Image getBackgroundImageFromPiece(int id)
+        {
+            return pieMenuPieces[id].transform.Find("CanvasPiePiece/PiePiece").GetComponent<Image>();
+        }
+
+        public void SetColorForPiece(int id, Color color)
+        {
+            getBackgroundImageFromPiece(id).color = color;
+        }
+
+        public void SetFillAmountForPiece(int id, float amount)
+        {
+            getBackgroundImageFromPiece(id).fillAmount = amount;
+        }
+
+        Image getIconFromPiece(int id)
+        {
+            return pieMenuPieces[id].transform.Find("CanvasIcon/Icon").GetComponent<Image>();
+        }
+
+        public void SetIconForPiece(int id, Sprite sprite)
+        {
+            getIconFromPiece(id).sprite = sprite;
+        }
+
+        public void SetLocalPositionForIcon(int id, Vector3 position)
+        {
+            getIconFromPiece(id).rectTransform.localPosition = position;
+        }
+
+        public void SetRotationForIcon(int id, Vector3 rotation, Space space)
+        {
+            getIconFromPiece(id).rectTransform.Rotate(rotation, space);
+        }
+
+        public void RotatePiece(int id, Vector3 rotation, Space space)
+        {
+            pieMenuPieces[id].transform.Rotate(rotation, space);
+        }
+
+        public Vector3 GetLocalScaleOfPiece(int id)
+        {
+            return pieMenuPieces[id].transform.localScale;
+        }
+
+        public void SetLocalScaleOfPiece(int id, Vector3 scale)
+        {
+            pieMenuPieces[id].transform.localScale = scale;
+        }
+
+        public void InstatiateMenuCursor()
+        {
+            instantiatedMenuCursor = Instantiate(menuCursor, transform);
+        }
+
+        public void SetGlobalPositionOfMenuCursor(Vector3 position)
+        {
+            instantiatedMenuCursor.transform.position = position;
+        }
+
+        public Vector3 GetLocalPositionOfCursor()
+        {
+            return instantiatedMenuCursor.transform.localPosition;
+        }
+
+        public void SetLocalPositionOfCursor(Vector3 position)
+        {
+            instantiatedMenuCursor.transform.localPosition = position;
+        }
+
     }
-    */
 }
