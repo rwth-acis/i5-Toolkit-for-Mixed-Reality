a870286b3ea3f9b3383896ee130d9a4a2f7b460d
EmilieHK <emiliehastrupkiil@gmail.com>
2021-09-19 11:13:44 +0200

    CleanUp of the Manager script

--- Assets/i5 Toolkit for Mixed Reality/Runtime/virtualEnvironments/Scripts/VirtualEnvironmentsManager.cs
+++ Assets/i5 Toolkit for Mixed Reality/Runtime/virtualEnvironments/Scripts/VirtualEnvironmentsManager.cs
@@ -10,10 +10,15 @@ using UnityEngine.UI;
 /// </summary>
 public class VirtualEnvironmentsManager : MonoBehaviour
 {
-
+    /// <summary>
+    /// Lists of asset bundles that are to be loaded. Contains information on the loading url and bundle name from both the server and local disk.
+    /// </summary>
     public EnvironmentLoadingInformation[] serverEnvironmentsFromInspector;
     public EnvironmentLoadingInformation[] localEnvironmentsFromInspector;
 
+    /// <summary>
+    /// Values for a default environment. The default environment is given by a skybox and a 3D model as prefab, as well as a preview image and credits.
+    /// </summary>
     public Material defaultSkybox;
     public GameObject defaultModel;
     public Sprite defaultPreviewImage;
@@ -21,28 +26,45 @@ public class VirtualEnvironmentsManager : MonoBehaviour
 
     private bool coroutinesFinished = false;
 
+    /// <summary>
+    /// The base URL for loading from a server and the base path to the local folder, respectively.
+    /// </summary>
     public string serverLoadingBaseURL;
     public string localLoadingBasePath;
 
+    /// <summary>
+    /// The final environment list
+    /// </summary>
     public List<EnvironmentData> environments = new List<EnvironmentData>();
 
 
    
     /// <summary>
-    /// TODO
+    /// Creates the environments list by first adding the default item and then loading the asset bundles from the server and the local disc, respectively.
     /// </summary>
     private void Awake()
     {
         if (UnityEngine.XR.WSA.HolographicSettings.IsDisplayOpaque)
-            //TODO add this, if default environment is added from inspector 
-            //environments.Add(new EnvironmentData("Default", defaultPreviewImage, defaultSkybox, defaultModel, defaultCredits, "");
-            StartCoroutine(GetAssetBundleObjectsFromServer());
-            StartCoroutine(GetAssetBundleObjectsFromLocal());
+            
+            if(defaultPreviewImage != null)
+            {
+                environments.Add(new EnvironmentData("Default", defaultPreviewImage, defaultSkybox, defaultModel, defaultCredits, "");
+            }
+
+            if (serverEnvironmentsFromInspector.Length > 0)
+            {
+                StartCoroutine(GetAssetBundleObjectsFromServer());
+            }
+
+            if (localEnvironmentsFromInspector.Length > 0)
+            {
+                StartCoroutine(GetAssetBundleObjectsFromLocal());
+            }
     }
 
 
     /// <summary>
-    /// TODO
+    /// Using a Web-Request, the asset bundles referenced in the serverEnvironmentsFromInspector list are loaded containing the skybox material, the 3D model as prefab, the preview image as sprite anf the credits of the creator for all used assets. This environment data is added to the list of environment.
     /// </summary>
     IEnumerator GetAssetBundleObjectsFromServer()
     {
@@ -87,7 +109,7 @@ public class VirtualEnvironmentsManager : MonoBehaviour
     }
 
     /// <summary>
-    /// TODO
+    /// Using a Web-Request, the asset bundles referenced in the localEnvironmentsFromInspector list are loaded from the disk containing the skybox material, the 3D model as prefab, the preview image as sprite anf the credits of the creator for all used assets. This environment data is added to the list of environment.
     /// </summary>
     IEnumerator GetAssetBundleObjectsFromLocal()
     {
