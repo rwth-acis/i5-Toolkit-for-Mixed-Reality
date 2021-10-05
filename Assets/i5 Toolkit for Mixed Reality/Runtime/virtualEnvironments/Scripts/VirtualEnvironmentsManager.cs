a508e280157addf4d8289c3a449c9fa43bae0018
EmilieHK <emiliehastrupkiil@gmail.com>
2021-10-05 10:54:18 +0200

    Added helpful functions to the main script

--- Assets/i5 Toolkit for Mixed Reality/Runtime/virtualEnvironments/Scripts/VirtualEnvironmentsManager.cs
+++ Assets/i5 Toolkit for Mixed Reality/Runtime/virtualEnvironments/Scripts/VirtualEnvironmentsManager.cs
@@ -28,6 +28,8 @@ namespace VirtualEnvironments
 
         private bool coroutinesFinished = false;
         private GameObject currentEnvironmentInstance;
+        private EnvironmentData currentEnvironment;
+        private bool defaultEnvironmentExists;
 
 
         /// <summary>
@@ -53,10 +55,12 @@ namespace VirtualEnvironments
                 {
                     environments.Add(new EnvironmentData("Default", defaultPreviewImage, defaultSkybox, defaultModel, defaultCredits, ""));
                     Debug.Log("Added the default virtual environment.");
+                    defaultEnvironmentExists = true;
                 }
                 else
                 {
                     Debug.Log("No default virtual environment has been added. For a default virtual environment to be added at least a preview image should be given.");
+                    defaultEnvironmentExists = false;
                 }
 
             if (serverEnvironmentsFromInspector.Length > 0)
@@ -143,6 +147,7 @@ namespace VirtualEnvironments
             yield return null;
         }
 
+
         /// <summary>
         /// Using a Web-Request, the asset bundles referenced in the localEnvironmentsFromInspector list are loaded from the disk containing the skybox material, the 3D model as prefab, the preview image as sprite anf the credits of the creator for all used assets. This environment data is added to the list of environment.
         /// </summary>
@@ -206,12 +211,12 @@ namespace VirtualEnvironments
             yield return null;
         }
 
-        /// <summary>
-        /// Instantiates the selected virtual environment after removing the currently active environment. Returns the current instance of the 3D model as prefab.
-        /// </summary>
-        /// <param name="selectedEnvironment">The EnvironmentData Item that should be instantiated</param>
 
-        public GameObject InstantiateEnvironment(EnvironmentData selectedEnvironment)
+        /// <summary>
+        /// Instantiates the selected virtual environment after removing the currently active environment. Returns the currently active environment.
+        /// </summary>
+        /// <param name="selectedEnvironment">The EnvironmentData Item that should be instantiated.</param>
+        public EnvironmentData InstantiateEnvironment(EnvironmentData selectedEnvironment)
         {
             if ((selectedEnvironment != null))
             {
@@ -224,6 +229,7 @@ namespace VirtualEnvironments
                 if (selectedEnvironment.EnvironmentBackground != null)
                 {
                     RenderSettings.skybox = selectedEnvironment.EnvironmentBackground;
+                    currentEnvironment = selectedEnvironment;
                 }
 
                 if (selectedEnvironment.EnvironmentPrefab != null)
@@ -231,7 +237,127 @@ namespace VirtualEnvironments
                     currentEnvironmentInstance = Instantiate(selectedEnvironment.EnvironmentPrefab, selectedEnvironment.EnvironmentPrefab.transform.position, selectedEnvironment.EnvironmentPrefab.transform.rotation);
                 }
             }
+            return currentEnvironment;
+        }
+
+
+        /// <summary>
+        /// Returns the list of environments that have been added from the inspector.
+        /// </summary>
+        public List<EnvironmentData> GetAllEnvironments()
+        {
+            return environments;
+        }
+
+
+        /// <summary>
+        /// Returns the instantiated 3D model of the currently active virtual environment.
+        /// </summary>
+        public GameObject GetCurrent3DModelInstance()
+        {
             return currentEnvironmentInstance;
+        }
+
+
+        /// <summary>
+        /// Returns the data of the currently active virtual environment.
+        /// </summary>
+        public EnvironmentData GetCurrentEnvironmentData()
+        {
+            return currentEnvironment;
+        }
+
+
+        /// <summary>
+        /// Allows the world space position of the currently instantiated 3D model to be set and changed, respectively.
+        /// </summary>
+        /// <param name="position"> The world space position the currently instantiated 3D Model should be changed to.</param>
+        public void SetCurrent3DModelPosition(Vector3 position)
+        {
+            currentEnvironmentInstance.transform.position = position;
+        }
+
+
+        /// <summary>
+        /// Allows the local scale of the currently instantiated 3D model to be set and changed, respectively.
+        /// </summary>
+        /// <param name="scale"> The local scale the currently instantiated 3D Model should be changed to.</param>
+        public void SetCurrent3DModelScale(Vector3 scale)
+        {
+            currentEnvironmentInstance.transform.localScale = scale;
+        }
+
+
+        /// <summary>
+        /// Allows the local rotation of the currently instantiated 3D model to be set and changed, respectively.
+        /// </summary>
+        /// <param name="rotation"> The local rotation the currently instantiated 3D Model should be changed to.</param>
+        public void SetCurrent3DModelRotation(Quaternion rotation)
+        {
+            currentEnvironmentInstance.transform.localRotation = rotation;
+        }
+
+
+        /// <summary>
+        /// Sets the current environment to the default environment.
+        /// </summary>
+        public void ReturnToDefaultEnvironment()
+        {
+            if (defaultEnvironmentExists)
+            {
+                InstantiateEnvironment(environments[0]);
+            }
+            else
+            {
+                Debug.Log("No default virtual environment has been added. Returning to it is not possible.");
+            }
+        }
+
+
+        /// <summary>
+        /// Removes the environment at an index in the list and returns the new list.
+        /// </summary>
+        /// <param name="index"> Index of the item that is to be removed.</param>
+        public List<EnvironmentData> RemoveEnvironmentAt(int index)
+        {
+            if (index >= 0 && index < environments.Count)
+            {
+                environments.RemoveAt(index);
+            }
+            else
+            {
+                Debug.Log("The given index is invalid. No environment has been deleted");
+            }
+            return environments;
+        }
+
+
+        /// <summary>
+        /// Removes a given environment from the environment list and returns the new list.
+        /// </summary>
+        /// <param name="environment"> Environment item that should be removed.</param>
+        public List<EnvironmentData> RemoveEnvironment(EnvironmentData environment)
+        {
+            environments.Remove(environment);
+            return environments;
+        }
+
+
+        /// <summary>
+        /// Reloads all items from the server that a given in the inspector.
+        /// </summary>
+        public void ReloadFromServer()
+        {
+            GetAssetBundleObjectsFromServer();
+        }
+
+
+        /// <summary>
+        /// Reloads all items from the local disc that a given in the inspector.
+        /// </summary>
+        public void ReloadFromLocal()
+        {
+            GetAssetBundleObjectsFromLocal();
         }
     }
 }
