7f045dc845a6a2448f4afc6a0245b793b663b038
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2021-11-02 17:22:27 +0100

    Added atest for setting the grip text

--- Assets/i5 Toolkit for Mixed Reality/Tests/Editor/PieMenu/ViveWandTeleporterTest.cs
+++ Assets/i5 Toolkit for Mixed Reality/Tests/Editor/PieMenu/ViveWandTeleporterTest.cs
@@ -5,20 +5,53 @@ using i5.Toolkit.MixedReality.PieMenu;
 using NUnit.Framework;
 using FakeItEasy;
 using Microsoft.MixedReality.Toolkit.Input;
+using UnityEngine.EventSystems;
 
 namespace i5.Toolkit.MixedReality.Tests.PieMenu
 {
-    //class testEnumerable : IEnumerable
-    //{
-    //    IEnu
-    //}
-
     public class ViveWandTeleporterTest
     {
         ViveWandTeleporterCore core = new ViveWandTeleporterCore();
 
         [Test]
-        public void SetupToolTest()
+        public void Setup_tool_new_grip_text()
+        {
+            IViveWandShell shell = A.Fake<IViveWandShell>();
+            string testText = "ThisIsATestText";
+
+
+
+            PieMenuSetup setup = A.Fake<PieMenuSetup>();
+            setup.defaultEntryTeleporter.gripSettings.textGrip = testText;
+
+
+            core.shell = shell;
+
+            A.CallTo(() => core.shell.GetPieMenuSetup()).Returns(setup);
+            core.SetupTool();
+            
+
+            string key = "GripText";
+
+            //The text for grip description must set
+            A.CallTo(() => shell.SetGameObjectActive("", false)).WhenArgumentsMatch(args =>
+                                                                                    args.Get<string>("key") == key &&
+                                                                                    args.Get<bool>("active")).
+                                                                                    MustHaveHappenedOnceExactly();
+            //Only one SetTextCall for the TextField
+            A.CallTo(() => shell.SetTMPText("", "")).WhenArgumentsMatch(args => args.Get<string>("key") == key).
+                                                                        MustHaveHappenedOnceExactly();
+            //The only SetTextCall for the TextField has to set the text to the testText
+            A.CallTo(() => shell.SetTMPText("", "")).WhenArgumentsMatch(args => args.Get<string>("key") == key && 
+                                                                        args.Get<string>("text") == testText).
+                                                                        MustHaveHappenedOnceExactly();
+
+
+        }
+
+
+        [Test]
+        public void Setup_tool_text_object_activation()
         {
             IViveWandShell shell = A.Fake<IViveWandShell>();
 
@@ -38,24 +71,12 @@ namespace i5.Toolkit.MixedReality.Tests.PieMenu
                                                                                     args.Get<string>("key") == "ButtonDescriptions" &&
                                                                                     args.Get<bool>("active")).
                                                                                     MustHaveHappenedOnceExactly();
-            //The text for grip description must set
-            A.CallTo(() => shell.AddGameobjectToBuffer("", "")).WhenArgumentsMatch(args =>
-                                                                                   args.Get<string>("name") == "ButtonDescriptions/GripText" &&
-                                                                                   args.Get<string>("key") == "GripText").
-                                                                                   MustHaveHappenedOnceExactly();
-            A.CallTo(() => shell.SetGameObjectActive("", false)).WhenArgumentsMatch(args =>
-                                                                                    args.Get<string>("key") == "GripText" &&
-                                                                                    args.Get<bool>("active")).
-                                                                                    MustHaveHappenedOnceExactly();
-            A.CallTo(() => shell.SetTMPText("", "")).WhenArgumentsMatch(args =>
-                                                                        args.Get<string>("key") == "GripText" &&
-                                                                        args.Get<string>("text") != "").
-                                                                        MustHaveHappenedOnceExactly();
 
             //The coroutine for deactivating the description texts again must be called
             A.CallTo(() => shell.SetGameObjectActive("", false)).WhenArgumentsMatch(args => args.Get<string>("key") == "ButtonDescriptions" &&
                                                                                             !args.Get<bool>("active")).
                                                                                             MustHaveHappenedOnceExactly();
         }
+
     }
 }
